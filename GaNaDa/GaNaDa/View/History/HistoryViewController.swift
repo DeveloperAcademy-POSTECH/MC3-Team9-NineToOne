324497ab90c56e8ea50b13d9c94b31dd66a403a6
feldblume5263 <feldblume5263@gmail.com>
2022-07-22 20:38:39 +0900

    feat: #17 collectionView autolayout 설정
    스토리보드에 collectionView 생성 후 autolayout 설정

--- GaNaDa/GaNaDa/View/History/HistoryViewController.swift
+++ GaNaDa/GaNaDa/View/History/HistoryViewController.swift
@@ -7,23 +7,45 @@
 
 import UIKit
 
-class HistoryViewController: UIViewController {
+final class HistoryViewController: UIViewController {
 
+    @IBOutlet weak var historyCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
 
-        // Do any additional setup after loading the view.
+    }
+
+}
+
+private extension HistoryViewController {
+    func configureCollectionView() {
+        historyCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        historyCollectionView.topAnchor.constraint(equalTo: view.topAnchor,
+                                                   constant: 140).isActive = true
+        historyCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        historyCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        historyCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -1 * (tabBarController?.tabBar.frame.height ?? .zero)).isActive = true
+        historyCollectionView.backgroundColor = .secondarySystemBackground
+    }
+}
+
+extension HistoryViewController: UICollectionViewDelegate {
+    
+}
+
+extension HistoryViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = QuizType2CollectionViewCell()
+        cell.backgroundColor = .cyan
+        
+        return cell
     }
-    */
-
+    
+    
 }
