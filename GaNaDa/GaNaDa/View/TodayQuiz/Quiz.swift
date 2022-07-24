9c091134dd320fd2af2f6301d6df311c5eeb64f8
limhyoseok <nonamedoud@gmail.com>
2022-07-25 01:42:47 +0900

    feat: 퀴즈 유형1 xib 파일 생성

---
+++ GaNaDa/GaNaDa/View/TodayQuiz/Quiz.swift
@@ -0,0 +1,15 @@
+//
+//  Quiz.swift
+//  GaNaDa
+//
+//  Created by hyo on 2022/07/25.
+//
+
+import Foundation
+
+struct Quiz {
+    let question: String
+    let type: String
+    let rightAnswer: String
+    let wrongAnswer: String
+}
---
+++ GaNaDa/GaNaDa/View/TodayQuiz/QuizType1.swift
@@ -0,0 +1,37 @@
+//
+//  QuizType1.swift
+//  GaNaDa
+//
+//  Created by hyo on 2022/07/24.
+//
+
+import Foundation
+import UIKit
+
+class QuizType1: UICollectionViewCell {
+    @IBOutlet weak var quizIndex: UILabel!
+    @IBOutlet weak var quizQuestion: UILabel!
+    @IBOutlet weak var rightAnswer: UIButton!
+    @IBOutlet weak var wrongAnswer: UIButton!
+
+    var data: Quiz = Quiz(question: "샘플 문장", type: "Blank", rightAnswer: "정답", wrongAnswer: "오답") {
+        didSet {
+            quizQuestion.text = makeQuizSentence(quiz: data)
+            rightAnswer.setTitle(data.rightAnswer, for: UIControl.State.normal)
+            wrongAnswer.setTitle(data.wrongAnswer, for: UIControl.State.normal)
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        contentView.layer.cornerRadius = 16
+    }
+}
+
+extension QuizType1 {
+    func makeQuizSentence(quiz: Quiz) -> String {
+        var quizQuestion = quiz.question
+        quizQuestion = quizQuestion.replacingOccurrences(of: "*", with: String(repeating: "_", count: quiz.rightAnswer.count + 2))
+        return quizQuestion
+    }
+}
