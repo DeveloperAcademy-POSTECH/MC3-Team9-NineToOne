e7b69bd035cc3ffa09e70fa730e38e54677a83bf
feldblume5263 <feldblume5263@gmail.com>
2022-07-24 16:25:09 +0900

    feat: #10 문제 유형 2 autolayout 적용
    autolayout을 간단하게 적용.
    하지만, cell의 width를 UIScreen으로 받아오는 방식은 수정 필요.

--- GaNaDa/GaNaDa/View/History/HistoryViewController.swift
+++ GaNaDa/GaNaDa/View/History/HistoryViewController.swift
@@ -7,27 +7,37 @@
 
 import UIKit
 
-final class HistoryViewController: UIViewController {
 
-    @IBOutlet weak var historyCollectionView: UICollectionView!
+final class HistoryViewController: UIViewController {
+    
+    @IBOutlet weak private var historyCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let flowLayout = historyCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        }
         configureCollectionView()
-
     }
-
+    
 }
 
 private extension HistoryViewController {
     func configureCollectionView() {
+        historyCollectionView.dataSource = self
+        historyCollectionView.delegate = self
+        historyCollectionView.register(QuizType2CollectionViewCell.self, forCellWithReuseIdentifier: QuizType2CollectionViewCell.id)
+        setCollectionViewLayout()
+        historyCollectionView.backgroundColor = .secondarySystemBackground
+    }
+    
+    func setCollectionViewLayout() {
         historyCollectionView.translatesAutoresizingMaskIntoConstraints = false
         historyCollectionView.topAnchor.constraint(equalTo: view.topAnchor,
                                                    constant: 140).isActive = true
         historyCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         historyCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         historyCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -1 * (tabBarController?.tabBar.frame.height ?? .zero)).isActive = true
-        historyCollectionView.backgroundColor = .secondarySystemBackground
     }
 }
 
@@ -37,15 +47,15 @@ extension HistoryViewController: UICollectionViewDelegate {
 
 extension HistoryViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = QuizType2CollectionViewCell()
-        cell.backgroundColor = .cyan
-        
+        guard let cell = historyCollectionView.dequeueReusableCell(withReuseIdentifier: QuizType2CollectionViewCell.id, for: indexPath) as? QuizType2CollectionViewCell
+        else {
+            return UICollectionViewCell()
+        }
+        cell.backgroundColor = .lightGray
         return cell
     }
-    
-    
 }
--- GaNaDa/GaNaDa/View/History/ViewComponents/QuizType2CollectionViewCell.swift
+++ GaNaDa/GaNaDa/View/History/ViewComponents/QuizType2CollectionViewCell.swift
@@ -9,22 +9,91 @@ import UIKit
 
 final class QuizType2CollectionViewCell: UICollectionViewCell {
     
-    private lazy var quizContentLabel: UILabel = UILabel()
+    private lazy var contentNumberLabel = UILabel()
+    private lazy var quizContentLabel = UILabel()
+    private lazy var contentAnswerAButton = UIButton()
+    private lazy var contentAnswerBButton = UIButton()
+    
+    static var id: String {
+        NSStringFromClass(Self.self).components(separatedBy: ".").last ?? "Error ID"
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configureCell()
+        configureComponents(frame: frame)
     }
     
     required init?(coder: NSCoder) {
         fatalError("Not implemented required init?(coder: NSCoder)")
     }
     
-    private func configureCell() {
+    private func configureComponents(frame: CGRect) {
+        configureContentView(frame: frame)
+        configureContentNumberLabel()
+        configureContentLabel()
+        configurecontentAnswerAButton()
+        configurecontentAnswerBButton()
+    }
+    
+    private func configureContentView(frame: CGRect) {
+        self.contentView.layer.cornerRadius = 16.0
+        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.contentView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 0.9),
+        ])
+    }
+    
+    private func configureContentNumberLabel() {
+        self.contentView.addSubview(contentNumberLabel)
+        contentNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentNumberLabel.text = "문제 2"
+        contentNumberLabel.font = UIFont.systemFont(ofSize: 14)
+        contentNumberLabel.textColor = .black
+        contentNumberLabel.textAlignment = .left
+        contentNumberLabel.sizeToFit()
+        NSLayoutConstraint.activate([
+            contentNumberLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 22),
+            contentNumberLabel.leftAnchor.constraint(equalTo: self.contentView.leftAnchor, constant: 24)
+        ])
+    }
+    
+    private func configureContentLabel() {
+        self.contentView.addSubview(quizContentLabel)
+        quizContentLabel.translatesAutoresizingMaskIntoConstraints = false
         quizContentLabel.text = "다음 중 맞는 것을 고르세요"
         quizContentLabel.font = UIFont.systemFont(ofSize: 14)
         quizContentLabel.textColor = .black
         quizContentLabel.textAlignment = .left
-        self.contentView.addSubview(quizContentLabel)
+        quizContentLabel.sizeToFit()
+        NSLayoutConstraint.activate([
+            quizContentLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 22),
+            quizContentLabel.leftAnchor.constraint(equalTo: self.contentNumberLabel.rightAnchor, constant: 12)
+        ])
+    }
+    
+    private func configurecontentAnswerAButton() {
+        self.contentView.addSubview(contentAnswerAButton)
+        contentAnswerAButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contentAnswerAButton.topAnchor.constraint(equalTo: self.contentNumberLabel.bottomAnchor, constant: 30),
+            contentAnswerAButton.leftAnchor.constraint(equalTo: self.contentView.leftAnchor, constant: 14),
+            contentAnswerAButton.rightAnchor.constraint(equalTo: self.contentView.rightAnchor, constant: -14),
+            contentAnswerAButton.heightAnchor.constraint(equalToConstant: 34)
+        ])
+        contentAnswerAButton.backgroundColor = .blue
+    }
+    
+    private func configurecontentAnswerBButton() {
+        self.contentView.addSubview(contentAnswerBButton)
+        contentAnswerBButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contentAnswerBButton.topAnchor.constraint(equalTo: self.contentAnswerAButton.bottomAnchor, constant: 8),
+            contentAnswerBButton.leftAnchor.constraint(equalTo: self.contentView.leftAnchor, constant: 14),
+            contentAnswerBButton.rightAnchor.constraint(equalTo: self.contentView.rightAnchor, constant: -14),
+            contentAnswerBButton.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -27),
+            contentAnswerBButton.widthAnchor.constraint(equalToConstant: 325),
+            contentAnswerBButton.heightAnchor.constraint(equalToConstant: 34)
+        ])
+        contentAnswerBButton.backgroundColor = .blue
     }
 }
