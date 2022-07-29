e79e450066b4e4752262267edbde8aefa461e902
moonjs0113 <littlemoom@naver.com>
2022-07-29 14:04:08 +0900

    feat: #41 Add LevelCase

--- GaNaDa/GaNaDa/Model/UserLevel.swift
+++ GaNaDa/GaNaDa/Model/UserLevel.swift
@@ -6,65 +6,46 @@
 //
 
 import Foundation
+import UIKit
 
-enum level: Int {
-    case lowNine = 0
-    case highNine
-    case lowEight
-    case highEight
-    case lowSeven
-    case highSeven
-    case lowSix
-    case highSix
-    case lowFive
-    case highFive
-    case lowFour
-    case highFour
-    case lowThree
-    case highThree
-    case lowTwo
-    case highTwo
-    case lowOne
-    case highOne
+enum LevelCase: String, CaseIterable {
+    case 학동
+    case 유생
+    case 급제
+    case 종9품
+    case 정9품
+    case 종8품
+    case 정8품
+    case 종7품
+    case 정7품
+    case 종6품
+    case 정6품
+    case 종5품
+    case 정5품
+    case 종4품
+    case 정4품
+    case 종3품
+    case 정3품
+    case 종2품
+    case 정2품
+    case 종1품
+    case 정1품
+    case 세종대왕
     
-    var name: String {
+    static func level(exp: Int) -> LevelCase {
+        if exp > self.allCases.count * 100 {
+            return .세종대왕
+        }
+        let index = exp % 100
+        return self.allCases[index]
+    }
+    
+    var levelImage: UIImage? {
         switch self {
-        case .lowNine:
-            return "종 9품"
-        case .highNine:
-            return "정 9품"
-        case .lowEight:
-            return "종 8품"
-        case .highEight:
-            return "정 8품"
-        case .lowSeven:
-            return "종 7품"
-        case .highSeven:
-            return "정 7품"
-        case .lowSix:
-            return "종 6품"
-        case .highSix:
-            return "정 6품"
-        case .lowFive:
-            return "종 5품"
-        case .highFive:
-            return "정 5품"
-        case .lowFour:
-            return "종 4품"
-        case .highFour:
-            return "정 4품"
-        case .lowThree:
-            return "종 3품"
-        case .highThree:
-            return "정 3품"
-        case .lowTwo:
-            return "종 2품"
-        case .highTwo:
-            return "정 2품"
-        case .lowOne:
-            return "종 1품"
-        case .highOne:
-            return "정 1품"
+        case .학동, .유생, .급제, .세종대왕:
+            return UIImage(named: self.rawValue)
+        default:
+            return UIImage(named: "폼")
         }
     }
 }
