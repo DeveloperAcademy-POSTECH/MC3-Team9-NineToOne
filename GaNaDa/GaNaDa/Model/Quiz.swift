9ce7929882eb69c1daa8c6b69a4030b2b5289f99
Somin Park <ramgoods2@gmail.com>
2022-07-30 19:56:59 +0900

    chore: 테스트 코드 정리
    ################
    # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지
    # 예)
    # feat: #42 로그인 email 텍스트 필드 추가
    # - 텍스트 필드의 emailText 변수는 LoginStateHolder와 연결되어 있습니다.
    # - 로그인 Password 텍스트 필드가 SecureField로 추가가 필요합니다.
    
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # docs: readme, git template, gitignore, lint file
    # refact : 코드 리팩토링
    # fix : 버그 수정
    # bug : 버그 제보
    # question: 궁금한 점
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # Resource : 코드를 제외한 리소스
    ################

--- GaNaDa/GaNaDa/Model/Quiz.swift
+++ GaNaDa/GaNaDa/Model/Quiz.swift
@@ -15,8 +15,6 @@ struct Quiz: Codable {
     let wrongAnswer: String
     var description: String = ""
     var example: [String] = []
-    var wasSolved: Bool = false
-    var isCorrect: Bool
 }
 
 enum QuizType: String, Codable {
@@ -25,66 +23,14 @@ enum QuizType: String, Codable {
 }
 
 extension Quiz {
-    static var preview: [Quiz] {
-        return [Quiz(quizID: 0,
+    static var preview: Quiz {
+        return Quiz(quizID: 0,
                     question: "무엇이 맞을까요?",
                     type: .choice,
                     rightAnswer: "뵙겠습니다",
                     wrongAnswer: "봽겠습니다",
                     description: "‘뵙겠습니다’가 맞습니다. ‘뵙겠습니다’의 기본형은 ‘뵙다(뵈다)’입니다.\n높임의 표현으로 ‘뵐까요?, 뵙도록 해요, 뵙죠’처럼 쓸 수 있습니다. ‘봬’는 ‘뵈어’의 준말입니다. ‘봬요(뵈어요), 뵀습니다(뵈었습니다)’처럼 ‘뵈’ 뒤에 ‘어’가 붙을 때는 이를 줄여서‘봬’로 쓸 수 있습니다. 따라서 ‘봴까요(X), 봽도록 해요(X), 봽죠(X)’ 모두 틀린 표기입니다.",
                     example: ["- 내일 2시에 뵙겠습니다.",
-                              "- 교수님을 뵈러 가요"],
-                    wasSolved: true,
-                    isCorrect: false),
-                Quiz(quizID: 1,
-                    question: "나는 iOS 개발자가 * 싶다.",
-                    type: .blank,
-                    rightAnswer: "되고",
-                    wrongAnswer: "돼고",
-                    description: "'되고'가 맞고 '돼고'는 안됩니다. 왜냐면 안되기때문이죠",
-                    example: ["나는 개발자가 되고싶다.",
-                              "나는 디자이너가 되고싶다."],
-                    wasSolved: true,
-                    isCorrect: false),
-                Quiz(quizID: 2,
-                    question: "나는 iOS 개발자가 * 싶다.",
-                    type: .blank,
-                    rightAnswer: "되고",
-                    wrongAnswer: "돼고",
-                    description: "'되고'가 맞고 '돼고'는 안됩니다. 왜냐면 안되기때문이죠",
-                    example: ["나는 개발자가 되고싶다.",
-                              "나는 디자이너가 되고싶다."],
-                    wasSolved: true,
-                    isCorrect: true),
-                Quiz(quizID: 4,
-                    question: "나는 iOS 개발자가 * 싶다.",
-                    type: .blank,
-                    rightAnswer: "되고",
-                    wrongAnswer: "돼고",
-                    description: "'되고'가 맞고 '돼고'는 안됩니다. 왜냐면 안되기때문이죠",
-                    example: ["나는 개발자가 되고싶다.",
-                              "나는 디자이너가 되고싶다."],
-                    wasSolved: false,
-                    isCorrect: false),
-                Quiz(quizID: 5,
-                    question: "무엇이 맞을까요?",
-                    type: .choice,
-                    rightAnswer: "뵙겠습니다",
-                    wrongAnswer: "봽겠습니다",
-                    description: "‘뵙겠습니다’가 맞습니다. ‘뵙겠습니다’의 기본형은 ‘뵙다(뵈다)’입니다.\n높임의 표현으로 ‘뵐까요?, 뵙도록 해요, 뵙죠’처럼 쓸 수 있습니다. ‘봬’는 ‘뵈어’의 준말입니다. ‘봬요(뵈어요), 뵀습니다(뵈었습니다)’처럼 ‘뵈’ 뒤에 ‘어’가 붙을 때는 이를 줄여서‘봬’로 쓸 수 있습니다. 따라서 ‘봴까요(X), 봽도록 해요(X), 봽죠(X)’ 모두 틀린 표기입니다.",
-                    example: ["- 내일 2시에 뵙겠습니다.",
-                              "- 교수님을 뵈러 가요"],
-                    wasSolved: true,
-                    isCorrect: true),
-                Quiz(quizID: 6,
-                    question: "무엇이 맞을까요?",
-                    type: .choice,
-                    rightAnswer: "뵙겠습니다",
-                    wrongAnswer: "봽겠습니다",
-                    description: "‘뵙겠습니다’가 맞습니다. ‘뵙겠습니다’의 기본형은 ‘뵙다(뵈다)’입니다.\n높임의 표현으로 ‘뵐까요?, 뵙도록 해요, 뵙죠’처럼 쓸 수 있습니다. ‘봬’는 ‘뵈어’의 준말입니다. ‘봬요(뵈어요), 뵀습니다(뵈었습니다)’처럼 ‘뵈’ 뒤에 ‘어’가 붙을 때는 이를 줄여서‘봬’로 쓸 수 있습니다. 따라서 ‘봴까요(X), 봽도록 해요(X), 봽죠(X)’ 모두 틀린 표기입니다.",
-                    example: ["- 내일 2시에 뵙겠습니다.",
-                              "- 교수님을 뵈러 가요"],
-                    wasSolved: false,
-                    isCorrect: false)]
+                              "- 교수님을 뵈러 가요"])
     }
 }
