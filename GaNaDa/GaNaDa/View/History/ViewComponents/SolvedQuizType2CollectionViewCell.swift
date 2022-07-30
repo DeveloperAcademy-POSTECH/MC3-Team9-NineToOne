1212b97ce41ffe1afc4566bd3c87ccbc4593d162
Somin Park <ramgoods2@gmail.com>
2022-07-30 21:12:34 +0900

    feat: #14 푼문제(choice)셀 추가
    ################
    # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지
    # 예)
    # feat: #42 로그인 email 텍스트 필드 추가
    # - 텍스트 필드의 emailText 변수는 LoginStateHolder와 연결되어 있습니다.
    # - 로그인 Password 텍스트 필드가 SecureField로 추가가 필요합니다.
    
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # docs: readme, git template, gitignore, lint file
    # refact : 코드 리팩토링
    # fix : 버그 수정
    # bug : 버그 제보
    # question: 궁금한 점
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # Resource : 코드를 제외한 리소스
    ################

--- GaNaDa/GaNaDa/View/History/ViewComponents/SolvedQuizType2CollectionViewCell.swift
+++ GaNaDa/GaNaDa/View/History/ViewComponents/SolvedQuizType2CollectionViewCell.swift
@@ -15,21 +15,43 @@ class SolvedQuizType2CollectionViewCell: QuizCollectionViewCell {
     @IBOutlet weak var vsTextLabel: UILabel!
     @IBOutlet weak var rightAnswerLabel: UILabel!
     @IBOutlet weak var quizIndexLabel: UILabel!
+    @IBOutlet weak var markImage: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         drawSolvedQuizeType1CellUI()
+        //MARK: 임시 레이아웃
+        NSLayoutConstraint.activate([
+            self.contentView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - QuizType2LayoutValue.Padding.cellHoriz * 2),
+            self.contentView.topAnchor.constraint(equalTo: self.topAnchor),
+            self.contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
     }
 }
 
 extension SolvedQuizType2CollectionViewCell {
     func drawSolvedQuizeType1CellUI() {
-        quizIndexLabel.textColor = UIColor.customColor(.customGray)
-        quizIndexLabel.font = UIFont.customFont(.subContent)
-        rightAnswerLabel.textColor = UIColor.systemOrange //custom color로 교체
-        rightAnswerLabel.font = UIFont.customFont(.customAnswer)
-        wrongAnswerLabel.font = UIFont.customFont(.customAnswer)
-        vsTextLabel.textColor = UIColor.customColor(.customGray)
-        vsTextLabel.font = UIFont.customFont(.content)
+        quizIndexLabel.textColor = .customColor(.customGray)
+        quizIndexLabel.font = .customFont(.subContent)
+        rightAnswerLabel.textColor = .customOrange
+        rightAnswerLabel.font = .customFont(.customAnswer)
+        wrongAnswerLabel.font = .customFont(.customAnswer)
+        vsTextLabel.textColor = .customColor(.customGray)
+        vsTextLabel.font = .customFont(.content)
+    }
+    
+    func setChoiceQuiz(indexPath: IndexPath) {
+        let quiz = HistoryQuiz.preview[indexPath.row]
+        quizIndexLabel.text = "\(indexPath.row + 1)"
+        rightAnswerLabel.text = quiz.rightAnswer
+        wrongAnswerLabel.text = quiz.wrongAnswer
+        wrongAnswerLabel.setTargetStringStrikeThrough(targetString: quiz.wrongAnswer, color: .black)
+        if quiz.status == 1 {
+            markImage.image = .init(named: "right")
+        }else {
+            markImage.image = .init(named: "wrong")
+        }
     }
 }
