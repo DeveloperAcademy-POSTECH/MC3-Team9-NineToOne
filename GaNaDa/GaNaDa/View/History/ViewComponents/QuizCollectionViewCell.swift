a1ba942392ed19a97beb086ff3c420cbcaa3692f
Somin Park <ramgoods2@gmail.com>
2022-07-31 23:58:44 +0900

    feat: #13 QuizCollectionViewCell 추가
    ################
    # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지
    # 예)
    # feat: #42 로그인 email 텍스트 필드 추가
    # - 텍스트 필드의 emailText 변수는 LoginStateHolder와 연결되어 있습니다.
    # - 로그인 Password 텍스트 필드가 SecureField로 추가가 필요합니다.
    
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # docs: readme, git template, gitignore, lint file
    # refact : 코드 리팩토링
    # fix : 버그 수정
    # bug : 버그 제보
    # question: 궁금한 점
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # Resource : 코드를 제외한 리소스
    ################

--- GaNaDa/GaNaDa/View/History/ViewComponents/QuizCollectionViewCell.swift
+++ GaNaDa/GaNaDa/View/History/ViewComponents/QuizCollectionViewCell.swift
@@ -14,28 +14,20 @@ struct QuizCellUIValue {
 }
 
 class QuizCollectionViewCell: UICollectionViewCell {
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        setCellUI()
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
     override func awakeFromNib() {
         super.awakeFromNib()
         setCellUI()
     }
-
-    
 }
 
 private extension QuizCollectionViewCell {
     func setCellUI() {
-        contentView.layer.applyShadow(color: UIColor.black, alpha: 0.1, x: 0, y: 5, blur: 4, spread: 0)
+        contentView.layer.applyShadow(color: UIColor.black, alpha: 0.1, x: 0, y: 4, blur: 20, spread: 0)
         contentView.backgroundColor = UIColor.white
         contentView.layer.borderWidth = 1
-        contentView.layer.borderColor = UIColor.orange.cgColor //커스텀컬러로 교체
+        contentView.layer.borderColor = UIColor.customOrange.cgColor
         contentView.layer.cornerRadius = QuizCellUIValue.CornerRadius.quizCell
+        self.clipsToBounds = false
+        self.layer.masksToBounds = false
     }
 }
