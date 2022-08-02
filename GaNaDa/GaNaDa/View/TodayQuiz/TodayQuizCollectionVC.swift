365a523e6d0e98014feefdaec4a506266fc9f652
Somin Park <ramgoods2@gmail.com>
2022-08-02 23:36:58 +0900

    feat: 해설화면에서 swipe로 전 화면으로 돌아가기
    ################
    # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지
    # 예)
    # feat: #42 로그인 email 텍스트 필드 추가
    # - 텍스트 필드의 emailText 변수는 LoginStateHolder와 연결되어 있습니다.
    # - 로그인 Password 텍스트 필드가 SecureField로 추가가 필요합니다.
    
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # docs: readme, git template, gitignore, lint file
    # refact : 코드 리팩토링
    # fix : 버그 수정
    # bug : 버그 제보
    # question: 궁금한 점
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # Resource : 코드를 제외한 리소스
    ################

--- GaNaDa/GaNaDa/View/TodayQuiz/TodayQuizCollectionVC.swift
+++ GaNaDa/GaNaDa/View/TodayQuiz/TodayQuizCollectionVC.swift
@@ -192,7 +192,7 @@ extension TodayQuizViewController: UICollectionViewDelegate {
                 }
             } else {
                 if let quizDetailViewController = storyboard.instantiateViewController(withIdentifier: "QuizDetailView") as? QuizDetailViewController {
-                    quizDetailViewController.prepareData(quiz : todayQuizs[indexPath.row])
+                    quizDetailViewController.prepareData(quiz : todayQuizs[indexPath.row], isSolved: true)
                     navigationController?.pushViewController(quizDetailViewController, animated: true)
                 }
             }
